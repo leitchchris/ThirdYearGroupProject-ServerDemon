2e347a6780b48b557534a13d1d0f276fef13e788
AJMKing <alasdair.king@me.com>
2013-03-30 16:17:38 +0000

    ammended main class with comand options for easyer testing

--- gpClientEmu/gpClientEmu/Main.cs
+++ gpClientEmu/gpClientEmu/Main.cs
@@ -10,21 +10,42 @@ namespace gpClientEmu
 
 		static void Main(string[] args)
 		{ 
-			/*
-			Messager client = new Messager ();
-			Console.WriteLine ("Please enter the ip of the server\n");
-			string ip = Console.ReadLine ();
-			client.Tx (ip, 2000);*/
-			//above is rubbish and neaded replaced
-
-			//call your class hear
-
-			//IDWorker = Kenect
-			IdWorker person = new IdWorker ();
-			person.PicRx ();
-			//MessageWorker = Droid
-
-			//EmbededWorker = Snap Net
+			try{
+				string opt = args[0];
+				
+				switch (opt) {
+				case "-M":
+				case "-m":
+					Console.WriteLine("Starting Messager");
+					Messager client = new Messager ();
+					Console.WriteLine ("Please enter the ip of the server\n");
+					string ip = Console.ReadLine ();
+					client.Tx (ip, 2000);
+					break;
+				case "-I":
+				case "-i":
+					Console.WriteLine("MOO");
+					IdWorker id = new IdWorker();
+					id.PicRx();
+					break;
+				case "-?":
+					Console.WriteLine("Hellp!");
+					Console.WriteLine("------");
+					Console.WriteLine("Start the program with thease options.");
+					Console.WriteLine("\r");
+					Console.WriteLine("Start Messeger: -M");
+					Console.WriteLine("Start Id Worker: -I");
+					break;
+				}
+			}
+			catch(IndexOutOfRangeException e){ //this is just to catch null option
+				Console.WriteLine("Hellp!");
+				Console.WriteLine("------");
+				Console.WriteLine("Start the program with thease options.");
+				Console.WriteLine("\r");
+				Console.WriteLine("Start Messeger: -M");
+				Console.WriteLine("Start Id Worker: -I");
+			}
 
 
 	
--- gpServer/gpServer/Main.cs
+++ gpServer/gpServer/Main.cs
@@ -4,25 +4,63 @@ using System.Text;
 using System.IO;
 using System.Drawing;
 
+namespace gpServer{
+	class MainClass{
+		static void Main(string[] args){
+			//this is to assist in testing all the classes, propbaly dont want this in the final thing when i get round to it. 
+			//probably some threding.
+			try{
+				string opt = args[0];
 
-namespace gpServer
-{
-	class MainClass
-	{
-		static void Main(string[] args)
-		{
-			/*
-			MessageWorker listner = new MessageWorker();
-			listner.Start();
-			/*EmbededWorker snap = new EmbededWorker ();
-			snap.SocketSendReceive ("192.168.0.65", 2000);*/
-			IdWorker person = new IdWorker();
-			person.PicTx ();
-
-
-
+				switch (opt) {
+				case "-Mw":
+				case "-MW":
+				case "-mw":
+				case "-mW":
+					Console.WriteLine("Starting MessageWorker");
+					MessageWorker listner = new MessageWorker();
+					listner.Start();
+					break;
+				case "-M":
+				case "-m":
+					Console.WriteLine("Starting Messager");
+					Messager mgr = new Messager();
+					mgr.Tx();
+					break;
+				case "-I":
+				case "-i":
+					Console.WriteLine("Starting ID Worker");
+					IdWorker id = new IdWorker();
+					id.PicTx();
+					break;
+				case "-E":
+				case "-e":
+					Console.WriteLine("Starting EmbededWorker");
+					EmbededWorker snap = new EmbededWorker ();
+					snap.SocketSendReceive ("192.168.0.65", 2000);
+					break;
+				case "-?":
+					Console.WriteLine("Hellp!");
+					Console.WriteLine("------");
+					Console.WriteLine("Start the program with thease options.");
+					Console.WriteLine("\r");
+					Console.WriteLine("Start MessageWorker: -Mw");
+					Console.WriteLine("Start Messeger: -M");
+					Console.WriteLine("Start Id Worker: -I");
+					Console.WriteLine("Start EmbededWorker: -E");
+					break;
+				}
+			}
+			catch(IndexOutOfRangeException e){ //this is just to catch null option
+				Console.WriteLine("Hellp!");
+				Console.WriteLine("------");
+				Console.WriteLine("Start the program with thease options.");
+				Console.WriteLine("\r");
+				Console.WriteLine("Start MessageWorker: -Mw");
+				Console.WriteLine("Start Messeger: -M");
+				Console.WriteLine("Start Id Worker: -I");
+				Console.WriteLine("Start EmbededWorker: -E");
+			}
 		}
-		
-
 	}
 }
